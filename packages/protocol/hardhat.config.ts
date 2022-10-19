6e934a898f0717815ef674ce41b10e927f0673a4
Ardiant Utomo <ardiant.utomo@gdplabs.id>
2022-10-19 10:14:45 +0700

    added dotenv

--- packages/protocol/hardhat.config.ts
+++ packages/protocol/hardhat.config.ts
@@ -1,6 +1,7 @@
 import "@nomiclabs/hardhat-ethers";
 import "@rumblefishdev/hardhat-kms-signer";
 import { HardhatUserConfig } from "hardhat/config";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
