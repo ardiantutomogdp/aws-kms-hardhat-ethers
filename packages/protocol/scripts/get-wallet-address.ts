58bd1f5e36307a79aa384ac61b3478367b57f607
Ardiant Utomo <ardiant.utomo@gdplabs.id>
2022-11-02 09:15:23 +0700

    update signer library

--- packages/protocol/scripts/get-wallet-address.ts
+++ packages/protocol/scripts/get-wallet-address.ts
@@ -1,25 +1,15 @@
 import { AlchemyProvider, getNetwork } from "@ethersproject/providers";
-import { AwsKmsSigner, AwsKmsSignerCredentials } from "ethers-aws-kms-signer";
+import { KMSSigner } from "@rumblefishdev/eth-signer-kms";
 require("dotenv").config();
 
 async function main() {
   const AWS_KMS_KEY_ID = process.env.AWS_KMS_KEY_ID || "";
-  const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || "";
-  const AWS_REGION = process.env.AWS_REGION || "";
-  const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || "";
   const ALCHEMY_KEY = process.env.ALCHEMY_KEY || "";
   const CHAIN_ID = process.env.CHAIN_ID || 1337;
 
-  const KMS_CREDENTIAL = {
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-    region: AWS_REGION,
-    keyId: AWS_KMS_KEY_ID,
-  } as AwsKmsSignerCredentials;
-
   const network = getNetwork(Number(CHAIN_ID));
-  const PROVIDER = new AlchemyProvider(network, ALCHEMY_KEY);
-  const signer = new AwsKmsSigner(KMS_CREDENTIAL).connect(PROVIDER);
+  const provider = new AlchemyProvider(network, ALCHEMY_KEY);
+  const signer = new KMSSigner(provider, AWS_KMS_KEY_ID).connect(provider);
   console.log("Wallet address: " + (await signer.getAddress()));
 }
 
--- packages/web2/src/main.ts
+++ packages/web2/src/main.ts
@@ -1,31 +1,20 @@
 import { AlchemyProvider, getNetwork } from "@ethersproject/providers";
+import { KMSSigner } from "@rumblefishdev/eth-signer-kms";
 import { ethers } from "ethers";
-import { AwsKmsSigner, AwsKmsSignerCredentials } from "ethers-aws-kms-signer";
 import HelloWorld from "../../protocol/artifacts/contracts/HelloWorld.sol/HelloWorld.json";
 require("dotenv").config();
 
 const AWS_KMS_KEY_ID = process.env.AWS_KMS_KEY_ID || "";
-const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || "";
-const AWS_REGION = process.env.AWS_REGION || "";
-const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || "";
 const ALCHEMY_KEY = process.env.ALCHEMY_KEY || "";
 const CHAIN_ID = process.env.CHAIN_ID || 1337;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || "";
 
 async function main() {
-  const KMS_CREDENTIAL = {
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-    region: AWS_REGION,
-    keyId: AWS_KMS_KEY_ID,
-  } as AwsKmsSignerCredentials;
-
   const network = getNetwork(Number(CHAIN_ID));
   const provider = new AlchemyProvider(network, ALCHEMY_KEY);
-  const signer = new AwsKmsSigner(KMS_CREDENTIAL).connect(provider);
+  const signer = new KMSSigner(provider, AWS_KMS_KEY_ID);
 
   console.log("Wallet address: " + (await signer.getAddress()));
-
   const contract = getContract(signer);
   let message = await contract["message"]();
   console.log("Initial message:" + message);
@@ -37,7 +26,7 @@ async function main() {
   console.log("current message:" + message);
 }
 
-function getContract(signer: AwsKmsSigner) {
+function getContract(signer: KMSSigner) {
   return new ethers.Contract(CONTRACT_ADDRESS, HelloWorld.abi, signer);
 }
 
